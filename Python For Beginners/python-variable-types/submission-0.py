d6c1c18d0e78fc2487c205c00450364bdf310c0c
Juan Gasparino <17909763+juan-gasparino@users.noreply.github.com>
2026-05-31 00:23:14 +0200

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,11 @@
+age = 25             # integer
+temperature = 98.6   # floating-point number
+is_true = True       # boolean
+name = "Alice"       # string
+my_list = [1, 2, 3]  # list
+
+print(type(age))
+print(type(temperature))
+print(type(is_true))
+print(type(name))
+print(type(my_list))
